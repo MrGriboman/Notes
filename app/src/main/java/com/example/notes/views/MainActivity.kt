84404a3b01a7896e760ab7aed8a11a68fb89d506
MrGriboman <96828637+MrGriboman@users.noreply.github.com>
2022-12-09 20:05:24 +1000

    fragment

--- app/src/main/java/com/example/notes/views/MainActivity.kt
+++ app/src/main/java/com/example/notes/views/MainActivity.kt
@@ -19,41 +19,19 @@ import com.example.notes.viewModels.TasksViewModelFactory
 
 class MainActivity : AppCompatActivity(), CreateTaskDialog.CreateTaskDialogInterface {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: TasksListAdapter
-    private lateinit var layoutManager: LinearLayoutManager
     private lateinit var viewModel: TasksViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val database = TasksDatabase.getInstance(this)
         val repository = TasksRepository(database)
         val viewModelFactory = TasksViewModelFactory(application, repository)
         viewModel = ViewModelProvider(this, viewModelFactory)[TasksViewModel::class.java]
 
-        adapter =
-            TasksListAdapter(viewModel.getAllTasks().value?.reversed() ?: listOf(), viewModel) {
-                Intent(this, EditTaskActivity::class.java).also { intent ->
-                    intent.putExtra("Task", TaskSerializer.fromTask(it))
-                    startActivity(intent)
-                }
-            }
-        layoutManager = LinearLayoutManager(this)
-        val tasksObserver = Observer<List<Task>> {
-            adapter.tasks = it.reversed()
-            adapter.notifyDataSetChanged()
-        }
-        viewModel.getAllTasks().observe(this, tasksObserver)
-
-        val itemTouchHelper = TasksItemTouchHelper(viewModel, adapter, binding.root)
-        itemTouchHelper.attachToRecyclerView(binding.rvTasks)
-        setContentView(binding.root)
-
         binding.apply {
-            rvTasks.adapter = adapter
-            rvTasks.layoutManager = layoutManager
-
             fbtnAdd.setOnClickListener {
                 CreateTaskDialog().show(supportFragmentManager, "Add task")
             }
